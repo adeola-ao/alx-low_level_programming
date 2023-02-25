2af2deb9fb790055a25dfd967d7dd1a2a507920f
adeola-ao <adeolaodumade@yahoo.com>
2023-02-24 20:25:13 -0800

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,14 @@
  *
  * @c: parameter to be printed with _putchar
  *
- * Return: Always 0
+ * Return: void
  */
 
 void print_numbers(void)
 {
 	int c;
 
-	for (c = 48; c <= 57; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
