8aed374e80b1baa10e71e193e8106347fbe275c7
Adeola Odumade <122860967+adeola-ao@users.noreply.github.com>
2023-02-27 23:38:17 +0100

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two integers a and b
+ *@a: integer 1
+ *@b: integer 2
+ * Return: void 
+ */
+
+void swap_int(int *a, int *b)
+{
+  int t;
+  t = *a;
+  *a = *b;
+  *b = t;
+}
