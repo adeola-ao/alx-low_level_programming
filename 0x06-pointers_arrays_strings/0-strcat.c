1a9c67fd165d50bd17ec4040b6f3386dba97a71a
Adeola Odumade <122860967+adeola-ao@users.noreply.github.com>
2023-03-02 16:03:32 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,29 @@
+#include "main.h"
+/**
+ *_strcat - function that concatenates two strings
+ *@dest: pointer to dest string
+ *@src: pointer to src string
+ *Return: pointer to dest
+ */
 
+char *_strcat(char *dest, char *src)
+{
+int a, b;
+
+a = 0;
+while (dest[a] != '\0')
+{
+a++;
+}
+
+b = 0;
+while (src[b] != '\0')
+{
+dest[a] = src[b];
+b++;
+a++;
+}
+dest[a] = '\0';
+
+return (dest);
+}
