e088c4a2f7a5a524ccc95174998182dda984d0ae
Adeola Odumade <122860967+adeola-ao@users.noreply.github.com>
2023-03-06 21:22:28 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-*_memset - function that fills memory with a constant byte
-*@s: pointer to fill
-*@b: constant byte
-*@n: number of byte
-*Return: new s pointer
-*/
+ *_memset - function that fills memory with a constant byte
+ *@s: pointer to fill
+ *@b: constant byte
+ *@n: number of byte
+ *Return: new s pointer
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
