a21b0c269f80caa0467c1acbefbef3327be7b349
Adeola Odumade <122860967+adeola-ao@users.noreply.github.com>
2023-03-10 22:50:17 +0100

    Create 3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strcmp - function that compares two strings
+ *@s1: pointer to string 1
+ *@s2: pointer to string 2
+ *Return: dest
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+
+char *str_one = s1;
+char *str_two = s2;
+
+
+while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
+{
+str_one++;
+str_two++;
+}
+
+return (*str_one - *str_two);
+}
