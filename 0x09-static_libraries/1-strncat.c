e38b4afe21acc0937f01b25ba9a323de5a86ade9
Adeola Odumade <122860967+adeola-ao@users.noreply.github.com>
2023-03-10 22:38:27 +0100

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,1 +1,30 @@
+#include "main.h"
+/**
+ *_strncat - function that concatenates two strings
+ *@dest: pointer to dest string
+ *@src: pointer to src string
+ *@n: value
+ *Return: pointer to dest
+ */
 
+char *_strncat(char *dest, char *src, int n)
+{
+int a, b;
+
+a = 0;
+while (dest[a] != '\0')
+{
+a++;
+}
+
+b = 0;
+while (b < n && src[b] != '\0')
+{
+dest[a] = src[b];
+b++;
+a++;
+}
+dest[a] = '\0';
+
+return (dest);
+}
