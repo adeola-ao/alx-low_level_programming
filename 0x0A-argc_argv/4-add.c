e99a06c577308308a263a3345200cec4f1602a8f
adeola-ao <adeolaodumade@yahoo.com>
2023-03-10 15:58:51 -0800

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* main - adds positive numbers
+*
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: 0 on success, 1 on failure
+*/
+
+/*
+ * The main function takes in the number of arguments and an array of strings.
+ * The first for loop iterates through the arguments.
+ * The second for loop iterates through each character of argument.
+ * If the character is not a digit, the program prints “Error” and returns
+ * If the character is a digit, the program adds the number to the sum.
+ * The program prints the sum and returns 0.
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
